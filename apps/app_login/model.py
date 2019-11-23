78bcb1727b1224e549c5cae17425fa37923cecb5
dannihong <462439535@qq.com>
2019-11-23 21:05:50 +0800

    1

---
+++ apps/app_login/model.py
@@ -0,0 +1,6 @@
+# -*-coding:utf8-*-
+from libs.db import db
+import datetime
+import pymysql
+
+
--- apps/app_login/views.py
+++ apps/app_login/views.py
@@ -1,6 +1,6 @@
 # -*-coding:utf8-*-
 from flask import Blueprint, render_template, request, jsonify
-
+from ..app_manager.model import get_users
 
 login_blue = Blueprint('login', __name__, template_folder='../../template', static_folder='../../static', url_prefix='/login')
 
@@ -27,9 +27,25 @@ def login():
     pwd = data['pwd']
     # 检查参数
     if not user:
-        return jsonify({"code": 200, "msg": u"user参数错误"})
+        return jsonify({"code": 200, "msg": u"user parameter null"})
     if not pwd:
-        return jsonify({"code": 200, "msg": u"pwd参数错误"})
+        return jsonify({"code": 200, "msg": u"pwd parameter null"})
+
+    # 用户是否存在
+    if len(get_users(user)) == 0:
+        return jsonify({"code": 200, "msg": "username error"})
+    # 判断密码是否正确（暂时不md5处理）
+    user_info = get_users(user)
+    password = user_info['password']
+    if password != pwd:
+        return jsonify({'code': 200, 'msg': 'password error'})
+
+    return jsonify({'code': 100, 'msg': 'login success'})
+
+
+
+
+
 
 
 
