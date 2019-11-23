e9c4fe9986f91ce15c74d0cdc722222c7d087709
dannihong <462439535@qq.com>
2019-11-23 19:38:15 +0800

    1

--- apps/app_manager/model.py
+++ apps/app_manager/model.py
@@ -1,10 +1,28 @@
 # -*-coding:utf8-*-
 from libs.db import db, cursor
+import datetime
 
 
-def get_users():
+def get_users(user):
     sql = 'select * from girls where status != 1001'
+    if user:
+        sql += ' and user = {}'.format(user)
     cursor.execute(sql)
     users = cursor.fetchall()
     cursor.close()
     return users
+
+
+def insert_user(user, pwd, nick):
+    status = 1000
+    create_time = current_time()
+    update_time = current_time()
+
+
+
+
+
+def current_time():
+    now = datetime.datetime.now()
+    now = now.strftime("%Y-%m-%d %H:%M:%S")
+    return now
--- apps/app_manager/views.py
+++ apps/app_manager/views.py
@@ -10,24 +10,36 @@ def test():
     return 'manager success'
 
 
+# 用户的用户名(user)不能重复
 @manager_blue.route('/register', methods=['post'])
 def manager_register():
     data = request.form
     user = data['user']
     pwd = data['pwd']
     nick = data['nick']
+    return jsonify({'nick': nick})
 
     if not user:
-        return jsonify({"code": 200, "msg": u"user参数错误"})
+        return jsonify({"code": 200, "msg": u"user parameter error"})
     if not pwd:
-        return jsonify({"code": 200, "msg": u"pwd参数错误"})
+        return jsonify({"code": 200, "msg": u"pwd parameter error"})
+
+    # 查找是否已经有相同user的会员
+    user = get_users(user)
+    if len(user) != 0:
+        return jsonify({"code": 200, "msg": u"user already exists"})
+    # 没有即新增会员信息
+
+
+
 
     pass  # 未完待续
 
 
+# 获取所有会员信息
 @manager_blue.route('/getuser', methods=['get', 'post'])
 def get_user():
-    users = get_users()
+    users = get_users('')
     return jsonify({"code": 200, "msg": u"成功", "data": users})
 
 
